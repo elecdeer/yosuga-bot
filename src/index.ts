8f146e08a629520ca5d7c2b63e4425048be0a570
elecdeer <deersub@gmail.com>
2020-10-24 16:39:31 +0900

    とりあえず喋るようになった 型を定めた async入れた

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import {Client, Message} from "discord.js";
+require("dotenv").config();
+
+
+import {Channel, Client, Message, TextChannel, VoiceConnection} from "discord.js";
 import {AxiosPromise, AxiosResponse} from "axios";
 
-require("dotenv").config();
 
 const Discord = require("discord.js");
 const client: Client = new Discord.Client();
@@ -9,19 +11,44 @@ const client: Client = new Discord.Client();
 const axios = require("axios").default;
 axios.defaults.baseURL = process.env.VOICEROID_DEAMON_URL;
 
-const URL = require("url");
+const async = require("async");
 
-type SessionState = any;
-type ServerConfig = any;
+// type SessionState = "Connecting" | "Disconnected"
+
+type SpeechParam = Partial<{
+	Text: string,
+	Kana: string,
+	Speaker: {
+		Volume: number,
+		Speed: number,
+		Emphasis: number,
+		PauseMiddle: number,
+		PauseLong: number,
+		PauseSentence: number
+	}
+}>
+
+
+type Session = {
+	// state: SessionState
+	connection: VoiceConnection,
+	textChannel: Channel
+};
+
+type ServerConfig = {
+	commandPrefix: string,
+	connectCommand: string,
+	disconnectCommand: string,
+};
 // interface SessionState{
 //
 // }
 
 //各ボイス接続時の状態など
-const sessionStateMap: {[channelId: string]: SessionState} = {};
+const sessionStateMap: Record<string, Session> = {};
 
 //各サーバ上でのbot設定
-const serverConfigMap: {[guildId: string]: ServerConfig} = {};
+const serverConfigMap: Record<string, Partial<ServerConfig>> = {};
 
 const defaultConfig: ServerConfig = {
 	commandPrefix: "yosuga",
@@ -49,25 +76,10 @@ client.on("message", async message => {
 	await handleText(message, sessionState, config);
 
 
-
-
-	// 	const url = `http://192.168.0.14:8080/api/speechtext/${encodeURI(message.content)}`;
-	//
-	// 	console.log("speakurl: " + url);
-	//
-	// 	connection.play(url);
-	// 	// connection.pla
-	// }
-	//
-	//
-	//
-	// console.log(message.content);
-	// // console.log(message.)
-
 })
 
 
-const handleCommand = async (message: Message, sessionState: SessionState, config: ServerConfig) => {
+const handleCommand = async (message: Message, sessionState: Session, config: ServerConfig) => {
 	const args = message.content.slice(config.commandPrefix.length).trim().split(" ");
 	const command = args.shift() || "";
 
@@ -94,27 +106,27 @@ const handleCommand = async (message: Message, sessionState: SessionState, confi
 	if(command === config.disconnectCommand){
 		console.log("disconnect");
 
-		if(!sessionState || !sessionState.connection) return;
+		if(!sessionState?.connection) return;
 
 		sessionState.connection.disconnect();
 
-		sessionStateMap[message.channel.id] = null;
+		delete sessionStateMap[message.channel.id];
 
 		return;
 	}
 }
 
 
-const handleText = async (message: Message, sessionState: SessionState, config: ServerConfig) => {
+const handleText = async (message: Message, sessionState: Session, config: ServerConfig) => {
 	//読み上げ
 	//ミドルウェアで加工する
 
-	console.log("read")
+	console.log("read");
 
 	if(!sessionState) return;
 
 	const text = message.content;
-	const param = {
+	const param: SpeechParam = {
 		"Text": text
 	}
 
@@ -129,8 +141,6 @@ const handleText = async (message: Message, sessionState: SessionState, config: 
 		data: param
 	})
 		.then((res: AxiosResponse) => {
-			console.log(res.data);
-
 			console.log("got wav");
 			sessionState.connection.play(res.data).once("finish", () => {
 				console.log("playFinish");
@@ -140,9 +150,12 @@ const handleText = async (message: Message, sessionState: SessionState, config: 
 			console.log(reason);
 			return;
 		})
-
-
 }
+
+
+
+
+
 
 
 client.login(process.env.DISCORD_TOKEN);
