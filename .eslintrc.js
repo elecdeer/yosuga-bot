bfef310ef709512fd8dc40e7f40aedcac3a2e1e6
averak <abe12@mccc.jp>
2021-03-26 02:25:38 +0900

    [chore] eslintrcの設定ファイルを作成 fix #5

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+    project: 'tsconfig.json',
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  rules: {},
+};
