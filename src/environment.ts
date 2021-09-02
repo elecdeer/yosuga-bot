39ab7a3096ba88a619d29a360b87582a3936c3cd
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-09-02 19:43:45 +0900

    refactor: 必要ない環境変数を削除

--- src/environment.ts
+++ src/environment.ts
@@ -8,11 +8,6 @@ export type YosugaEnv = {
   discordToken: string;
   discordAppId: string;
   discordPublicKey: string;
-  voiceroidDaemonUrl?: string;
-  assistantSeikaUrl?: string;
-  assistantSeikaBasicUser?: string;
-  assistantSeikaBasicPassword?: string;
-  socketIOAudioRecorderWSUrl: string;
 };
 
 export type ImageEnv = {
@@ -29,41 +24,10 @@ const initEnv = (): YosugaEnv => {
     discordToken: process.env.DISCORD_TOKEN,
     discordAppId: process.env.DISCORD_APP_ID,
     discordPublicKey: process.env.DISCORD_PUB_KEY,
-    voiceroidDaemonUrl: process.env.VOICEROID_DAEMON_URL,
-    assistantSeikaUrl: process.env.ASSISTANT_SEIKA_URL,
-    assistantSeikaBasicUser: process.env.ASSISTANT_SEIKA_BASIC_USER,
-    assistantSeikaBasicPassword: process.env.ASSISTANT_SEIKA_BASIC_PASSWORD,
-    socketIOAudioRecorderWSUrl: process.env.SOCKETIO_AUDIO_RECORDER_WS_URL,
   };
 
   if (!env.configPath) env.configPath = "./config";
   if (!env.discordToken) throw Error("環境変数 DISCORD_TOKEN が設定されていません");
-  if (!env.voiceroidDaemonUrl) {
-    console.warn(
-      "環境変数 VOICEROID_DAEMON_URL が設定されていないためvoiceroidDaemonによる読み上げができません"
-    );
-  }
-  if (!env.assistantSeikaUrl) {
-    console.warn(
-      "環境変数 ASSISTANT_SEIKA_URL が設定されていないためassistantSeikaによる読み上げができません"
-    );
-  }
-  if (!env.assistantSeikaBasicUser) {
-    console.warn(
-      "環境変数 ASSISTANT_SEIKA_BASIC_USER が設定されていないためassistantSeikaによる読み上げができません"
-    );
-  }
-  if (!env.assistantSeikaBasicPassword) {
-    console.warn(
-      "環境変数 ASSISTANT_SEIKA_BASIC_PASSWORD が設定されていないためassistantSeikaによる読み上げができません"
-    );
-  }
-  if (!env.socketIOAudioRecorderWSUrl) {
-    console.warn(
-      "環境変数 SOCKERIO_AUDIO_RECORDER_WS_URL が設定されていないためassistantSeikaによる読み上げができません"
-    );
-    env.socketIOAudioRecorderWSUrl = "";
-  }
 
   console.info(env);
 
