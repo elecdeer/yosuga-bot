20a7e9762bd42bf763414cd46bf147108fc148fe
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-12-31 17:28:35 +0900

    feat: addVoicevoxSubの追加まで

---
+++ src/commands/configSubCommands/addVoicevoxSub.ts
@@ -0,0 +1,44 @@
+import { CommandInteraction } from "discord.js";
+
+import { ConfigEachLevel, MasterLevel } from "../../config/typesConfig";
+import { SetConfigSubCommand } from "./setConfigSubCommand";
+
+export class AddVoicevoxSub extends SetConfigSubCommand<MasterLevel, "speakerBuildOptions"> {
+  constructor(level: MasterLevel) {
+    super(
+      {
+        name: "add-voicevox",
+        description: "VOICEVOXによるボイスの追加",
+        options: [
+          {
+            name: "styleName",
+            description: "ボイスの登録名",
+            type: "STRING",
+            required: true,
+          },
+          {
+            name: "url",
+            description: "VOICEVOXエンジンのURLBase",
+            type: "STRING",
+            required: true,
+          },
+          {
+            name: "styleId",
+            description: "追加したいspeakerのstyleのId",
+            type: "STRING",
+            required: true,
+          },
+        ],
+      },
+      level,
+      "speakerBuildOptions"
+    );
+  }
+
+  protected getValueFromOptions(
+    options: CommandInteraction["options"],
+    oldValue: Readonly<ConfigEachLevel<MasterLevel>["speakerBuildOptions"]> | undefined
+  ): ConfigEachLevel<MasterLevel>["speakerBuildOptions"] | undefined {
+    return undefined;
+  }
+}
