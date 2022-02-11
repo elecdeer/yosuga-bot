4d9adc280cfc9942e979a1c2a79aeeef037789d2
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-02-11 21:12:43 +0900

    fix: テスト間違っていた

--- src/processor/englishRead/romajiToKana.test.ts
+++ src/processor/englishRead/romajiToKana.test.ts
@@ -1,25 +1,43 @@
 import { romajiToJpRead } from "./romajiToKana";
 
 test("ローマ字→かな#1", () => {
-  expect(romajiToJpRead("aiueo")).toBe("あいうえお");
+  expect(romajiToJpRead("aiueo")).toEqual({
+    kana: "あいうえお",
+    complete: true,
+  });
 });
 
 test("ローマ字→かな#2", () => {
-  expect(romajiToJpRead("wagahaihanekodearu")).toBe("わがはいはねこである");
+  expect(romajiToJpRead("wagahaihanekodearu")).toEqual({
+    kana: "わがはいはねこである",
+    complete: true,
+  });
 });
 
 test("ローマ字→かな#3", () => {
-  expect(romajiToJpRead("inta-netto")).toBe("いんたーねっと");
+  expect(romajiToJpRead("inta-netto")).toEqual({
+    kana: "いんたーねっと",
+    complete: true,
+  });
 });
 
 test("ローマ字→かな#4", () => {
-  expect(romajiToJpRead("fattosanjuuni")).toBe("ふぁっとさんじゅうに");
+  expect(romajiToJpRead("fattosanjuuni")).toEqual({
+    kana: "ふぁっとさんじゅうに",
+    complete: true,
+  });
 });
 
 test("ローマ字→かな#5", () => {
-  expect(romajiToJpRead("mannindensha")).toBe("まんいんでんしゃ");
+  expect(romajiToJpRead("mannindensha")).toEqual({
+    kana: "まんいんでんしゃ",
+    complete: true,
+  });
 });
 
 test("ローマ字→かな#6", () => {
-  expect(romajiToJpRead("english")).toBe("えんglいsh");
+  expect(romajiToJpRead("english")).toEqual({
+    kana: "えんglいsh",
+    complete: false,
+  });
 });
