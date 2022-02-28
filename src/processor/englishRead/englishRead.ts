f3c32473e359396fd00340109595b3d7f6390529
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-03-01 01:50:38 +0900

    feat: 単語分割してローマ字置換するのを実装

--- src/processor/englishRead/englishRead.ts
+++ src/processor/englishRead/englishRead.ts
@@ -1,7 +1,28 @@
+import TinySegmenter from "tiny-segmenter";
+
+import { romajiToJpRead } from "./romajiToKana";
+
 export const replaceEnglishRead = (input: string): string => {
-  return input;
+  const words = splitByWord(input);
+  return words
+    .map((word) => {
+      const { complete, kana } = romajiToJpRead(word);
+      if (complete) {
+        return kana;
+      } else {
+        return word;
+      }
+    })
+    .join("");
+};
+
+const segmenter = new TinySegmenter();
+const splitByWord = (input: string): string[] => {
+  return segmenter.segment(input);
 };
 
 // export const englishToJpRead = (englishStr: string): string => {
 //   return englishStr;
 // };
+
+//Mecabを使って分割します
