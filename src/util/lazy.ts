9a19369a5ab2bf6adb159aa5f80df123a76e4f5b
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-06-06 03:39:35 +0900

    feat: resolveLazyをundefinedも処理するように

--- src/util/lazy.ts
+++ src/util/lazy.ts
@@ -1,6 +1,12 @@
 export type Lazy<T> = T | (() => T);
 
-export const resolveLazy = <T>(lazy: Lazy<T>): T => {
+type ResolveLazy = {
+  <T>(lazy: Lazy<T>): T;
+  <T>(lazy: Lazy<T> | undefined): T | undefined;
+};
+
+export const resolveLazy: ResolveLazy = (lazy) => {
+  if (lazy === undefined) return undefined;
   if (lazy instanceof Function) {
     return lazy();
   } else {
