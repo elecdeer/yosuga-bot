92896170ce7e138512223cd0fd8a5cb6cb0039b8
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-07-24 14:51:09 +0900

    feat: logのフォーマットを変更

---
+++ src/logger.ts
@@ -0,0 +1,76 @@
+import log4js from "log4js";
+import path from "path";
+import * as util from "util";
+
+import { yosugaEnv } from "./environment";
+
+import type { Layout } from "log4js";
+
+const logLayout = (oneLine: boolean): Layout => ({
+  type: "pattern",
+  pattern: "%d %z %p %c %f:%l %x{oneLine}",
+  tokens: {
+    oneLine: (logEvent) => {
+      return logEvent.data
+        .map((d) => {
+          if (oneLine) {
+            return stringifyLogMessage(d).replace(/\n/gm, "\\n");
+          } else {
+            const str = stringifyLogMessage(d);
+            if (str.includes("\n")) {
+              const lines = str.split("\n");
+              return `\n${lines.map((line) => `  ${line}`).join("\n")}`;
+            } else {
+              return str;
+            }
+          }
+        })
+        .filter((d: string) => d.length > 0)
+        .join(" ");
+    },
+  },
+});
+
+const stringifyLogMessage = (data: unknown) => {
+  if (typeof data === "string" || typeof data === "number" || typeof data === "boolean") {
+    return data.toString();
+  } else {
+    return util.inspect(data, { breakLength: Infinity });
+  }
+};
+
+export const initLogger = () => {
+  const layoutOneLine = logLayout(true);
+  const layoutMultiLine = logLayout(false);
+
+  log4js.configure({
+    appenders: {
+      out: { type: "stdout", layout: layoutMultiLine },
+      app: {
+        type: "file",
+        filename: path.join(yosugaEnv.logDir, "yosuga.log"),
+        layout: layoutOneLine,
+        pattern: "-yyyy-MM-dd",
+        dayToKeep: 7,
+        compress: true,
+      },
+      error: {
+        type: "file",
+        filename: path.join(yosugaEnv.logDir, "yosuga-error.log"),
+        layout: layoutMultiLine,
+      },
+    },
+    categories: {
+      default: {
+        appenders: ["out", "app"],
+        level: "all",
+        enableCallStack: true,
+      },
+      error: {
+        appenders: ["error"],
+        level: "error",
+        enableCallStack: true,
+      },
+    },
+  });
+};
