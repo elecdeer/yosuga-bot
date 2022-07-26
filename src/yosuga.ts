83404e4e349d4c17641935a5e1ecee88ca16900b
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-07-26 13:35:21 +0900

    feat: eventなどをまとめるyosugaクラスを実装

---
+++ src/yosuga.ts
@@ -0,0 +1,74 @@
+import { getLogger } from "log4js";
+
+import { createEventFlow } from "./util/eventFlow";
+
+import type { IEventFlow } from "./util/eventFlow";
+import type { Message, VoiceState, Client, Interaction } from "discord.js";
+import type { Logger } from "log4js";
+
+type YosugaEvent<T extends Record<string, unknown>> = IEventFlow<
+  {
+    yosuga: Yosuga;
+    logger: Logger;
+  } & T
+>;
+
+export class Yosuga {
+  private readonly client: Client<true>;
+
+  readonly events: {
+    messageCreate: YosugaEvent<{
+      message: Message;
+    }>;
+    interactionCreate: YosugaEvent<{
+      interaction: Interaction;
+    }>;
+    voiceStateUpdate: YosugaEvent<{
+      oldState: VoiceState;
+      newState: VoiceState;
+    }>;
+  } = {
+    messageCreate: createEventFlow(),
+    interactionCreate: createEventFlow(),
+    voiceStateUpdate: createEventFlow(),
+  };
+
+  constructor(client: Client<true>) {
+    this.client = client;
+
+    this.warpEvents();
+    this.registerLogHandlers();
+  }
+
+  private warpEvents() {
+    this.client.on("messageCreate", (message: Message) => {
+      const logger = getLogger("messageCreate");
+      logger.addContext("eventId", message.id);
+      this.events.messageCreate.emit({ yosuga: this, message, logger });
+    });
+
+    this.client.on("interaction", (interaction: Interaction) => {
+      const logger = getLogger("interactionCreate");
+      logger.addContext("eventId", interaction.id);
+      this.events.interactionCreate.emit({ yosuga: this, interaction, logger });
+    });
+
+    this.client.on("voiceStateUpdate", (oldState: VoiceState, newState: VoiceState) => {
+      const logger = getLogger("voiceStateUpdate");
+      logger.addContext("eventId", newState.id);
+      this.events.voiceStateUpdate.emit({ yosuga: this, oldState, newState, logger });
+    });
+  }
+
+  private registerLogHandlers() {
+    this.events.messageCreate.on(({ logger }) => {
+      logger.debug("event called: messageCreate");
+    });
+    this.events.interactionCreate.on(({ logger }) => {
+      logger.debug("event called: interactionCreate");
+    });
+    this.events.voiceStateUpdate.on(({ logger }) => {
+      logger.debug("event called: voiceStateUpdate");
+    });
+  }
+}
