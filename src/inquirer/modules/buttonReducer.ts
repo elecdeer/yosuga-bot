3a9fc56a5aa47cc7163eff69b39ff490813c9b73
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-09-08 17:47:13 +0900

    fix: buttonReducer.tsでaction.typeが規定外の場合エラーを出すように

--- src/inquirer/modules/buttonReducer.ts
+++ src/inquirer/modules/buttonReducer.ts
@@ -5,5 +5,5 @@ export const buttonReducer: StateReducer<number, ButtonAction> = (prev, action) 
   if (action.type === "click") {
     return prev + 1;
   }
-  return prev;
+  throw new Error("Invalid action type");
 };
